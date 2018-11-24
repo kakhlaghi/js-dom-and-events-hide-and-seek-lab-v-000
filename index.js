a91d0d5adfea0e6ba898ef4f7eb9e256430519a4
Kevin Akhlaghi <kevin.akhlaghi@gmail.com>
2018-11-24 16:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
   for (let i = 0, l = rankedLists.length; i < l; i++) {
     let children = rankedLists[i].children
-    for (let j = 0, k = children.length; j < k; j++) {
+    for (let q = 0, w = children.length; q < w; q++) {
       children[j].innerHTML = parseInt(children[j].innerHTML) + n
     }
   }
